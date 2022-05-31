ef6abd766c6b6c41dd4496eca7a2b008cf087238
Pavanarathod <edrocullen13@gmail.com>
2022-05-31 22:12:15 +0530

    responsive trying

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,14 +5,17 @@ import DropDown from "./TaskTwo/DropDown";
 import {
   ChevronDownIcon,
   GlobeAltIcon,
+  MenuAlt3Icon,
   SearchIcon,
   UserIcon,
+  XIcon,
 } from "@heroicons/react/solid";
 import { useRouter } from "next/router";
 
 const Header = () => {
   const router = useRouter();
   const [selectedOncologist, setSelectedOncologist] = useState("");
+  const [toggle, setToggle] = useState(false);
   const oncologistOptions = [
     "Doctors near me",
     "International experts",
@@ -31,7 +34,7 @@ const Header = () => {
   return (
     <>
       <header className="bg-gray-50 h-[126px] flex border-b border-gray-200 shadow-md sticky top-0">
-        <nav className="container mx-auto px-4 flex items-center space-x-24">
+        <nav className="container relative mx-auto px-4 flex items-center space-x-24 justify-between">
           <div>
             <Link href="/">
               <img
@@ -41,7 +44,7 @@ const Header = () => {
               />
             </Link>
           </div>
-          <div className="flex-1 flex items-center space-x-5 text-sm text-gray-500">
+          <div className="hidden flex-1 lg:inline-flex items-center space-x-5 text-sm text-gray-500">
             {/* ITEMS */}
             <DropDown
               selected={selectedOncologist}
@@ -101,6 +104,82 @@ const Header = () => {
               </div>
             </div>
           </div>
+
+          {/* MENU ITEMS */}
+
+          <div className="lg:hidden  ">
+            {toggle ? (
+              <XIcon
+                onClick={() => setToggle(false)}
+                className="h-8 transition-all delay-100 duration-300 ease-in-out"
+              />
+            ) : (
+              <MenuAlt3Icon onClick={() => setToggle(true)} className="h-8" />
+            )}
+          </div>
+
+          {toggle && (
+            <div className="absolute bg-red-500 w-full right-24 flex flex-col top-36 justify-center items-center space-x-5 text-sm text-gray-500">
+              {/* ITEMS */}
+              <DropDown
+                selected={selectedOncologist}
+                setSelected={setSelectedOncologist}
+                defaultValue={"Oncologist"}
+                optionValue={oncologistOptions}
+              />
+              <a
+                href="/hospital-service"
+                className={`${
+                  router.pathname === "/hospital-service"
+                    ? "border-b-4 mt-2 pb-1 border-[#007E84]"
+                    : ""
+                }`}
+              >
+                Hospital Services
+              </a>
+              <a
+                href="/diagnosis"
+                className={`${
+                  router.pathname === "/diagnosis"
+                    ? "border-b-4 mt-2 pb-1 border-[#007E84]"
+                    : ""
+                }`}
+              >
+                Diagnosis
+              </a>
+              <DropDown
+                selected={selectedOncologist}
+                setSelected={setSelectedOncologist}
+                defaultValue={"Home Service"}
+                optionValue={homeServiceOptions}
+              />
+              <Link href="/">Financial guidence</Link>
+              <Link href="/">Wellness package</Link>
+              <div className="flex items-center divide-x-2 divide-gray-600">
+                <div className="pr-3">
+                  <SearchIcon className="h-5" />
+                </div>
+                <div className="flex items-center space-x-2 pl-3">
+                  <p>Providers</p>
+                  <ChevronDownIcon className="h-5" />
+                </div>
+              </div>
+
+              {/* BUTTONS */}
+              <div className="flex items-center flex-col space-x-5">
+                <div className="flex items-center space-x-2 border border-gray-500 px-1 rounded-full py-[2px]">
+                  <UserIcon className="h-8 px-2 py-2 border border-gray-400 rounded-full" />
+                  <span className="text-sm pr-2 truncate">Pavan</span>
+                </div>
+                <button className="px-6 py-2 bg-[#007E84] text-white rounded-full">
+                  Download now
+                </button>
+                <div>
+                  <GlobeAltIcon className="h-10 px-2 py-2 border text-[#007E84] bg-gray-100 border-[#007E84] rounded-full" />
+                </div>
+              </div>
+            </div>
+          )}
         </nav>
       </header>
     </>
